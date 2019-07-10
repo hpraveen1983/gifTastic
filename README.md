bbb7d9b09599ebe4b97fb539eed579e081cca0d2
hpraveen <hariharanster@gmail.com>
2019-07-10 00:27:06 -0400

    Few Final changes

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# gifTastic
+gifTastic
+
+Instructions:
+
+In this assignment, a GIPHY API was used to make a dynamic web page that populates gifs of my choice. 
+
+Getting Started
+Click on the link to see the finished website!
+https://hpraveen1983.github.io/gifTastic/
+
+
+Technologies:
+
+HTML5
+CSS3
+Js
+jQuery
+Bootstrap
+AJAX
+API
--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,4 +1,4 @@
-var athletesArray = ["Sachin Tendulkar", "Virat Kohli", "Ricky Ponting", "Steve Waugh", "Brian Lara"];
+var athletesArray = ["Roger Federer", "Novak Djokovic","Rafael Nadal","Serena Williams"];
 
 $(document).ready(function() {
     for (var i = 0; i < athletesArray.length; i++) {
@@ -6,3 +6,50 @@ $(document).ready(function() {
     }
 });
 
+function athleteButtonClicked() {
+    var userInput = $('#athlete-input').val();
+    searchGif(userInput);
+}
+
+function submitButtonClicked() {
+    var userInput = $('#athlete-input').val();
+
+    if (userInput) {
+        $('#athlete-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+    }
+}
+
+function searchGif(gifName) {
+    $.ajax({
+            url: "https://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=dc6zaTOxFJmzC&limit=10",
+            type: 'GET',
+        })
+        .done(function(response) {
+            displayGif(response);
+        })
+}
+
+function displayGif(response) {
+    $('#athletes').empty();
+    for (var i = 0; i < response.data.length; i++) {
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
+
+        image = '<div class="col-md-4">' + image + "</div>";
+        $('#athletes').append(image);
+    }
+
+    $('.movImage').on('click', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+
+    });
+}
